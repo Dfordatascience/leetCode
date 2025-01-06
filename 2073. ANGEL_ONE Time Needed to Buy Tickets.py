9b621809ef6c0ecfd4fe45f544533657f7e3a64e
vishaljadhavutube <vishaljadhavutube@gmail.com>
2025-01-06 09:02:59 +0530

    20. valid parenthesis

---
+++ 2073. ANGEL_ONE Time Needed to Buy Tickets.py
@@ -0,0 +1,11 @@
+# https://www.youtube.com/watch?v=cVmS9N6kf2Y&ab_channel=NeetCodeIO
+
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        res = 0
+        for i in range(len(tickets)):
+            if i <=k:
+                res +=min(tickets[i], tickets[k])
+            else:
+                res += min(tickets[i], tickets[k]-1)
+        return res
